ba3973fb6532f2c921fa7240674a4253525cacf1
HoussamCbk <choubikhoussam@gmail.com>
2024-09-15 11:27:56 +0100

    yes

--- next.config.mjs
+++ next.config.mjs
@@ -2,39 +2,19 @@ import { withSentryConfig } from '@sentry/nextjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: "export", // Explicitly define export to be injected correctly
 };
 
 const sentryWebpackPluginOptions = {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
     silent: true,
     org: "javascript-mastery",
     project: "javascript-nextjs",
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
 };
 
+// Use withSentryConfig to wrap the nextConfig, keeping all properties
 export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
