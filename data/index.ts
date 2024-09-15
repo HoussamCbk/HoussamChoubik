4de1fec2e77710597ed1b7f9ebbdd0bb6a02b96b
HoussamCbk <choubikhoussam@gmail.com>
2024-09-15 11:00:28 +0100

    changes

---
+++ data/index.ts
@@ -0,0 +1,81 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Hospital Automatisation",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "./p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://www.youtube.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "./p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/see more",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Major",
+    desc1: "Engineering",
+    desc2: "ENSAM Casablanca",
+    className: "md:col-span-2",
+    thumbnail: "./1.svg",
+  },
+  {
+    id: 2,
+    title: "Baccalauréat",
+    desc1: "Sc Maths A Biof",
+    desc2: "Lycée Newton Mohammedia",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./2.svg",
+  },
+  {
+    id: 3,
+    desc1:
+      "Hello there, I am Choubik Houssam an engineering student and presently studying at ENSAM Casablanca while bearing immense focus on my preparatory years. Besides academic activities, I am also involved in A&M Mechatronics club which is a robotics and mechanics club. I value diligence as I am always a hardworking and a go-getter person who approaches everything with ambition.",
+
+    className: "md:col-span-2", // change to md:col-span-2
+  },
+  {
+    id: 3,
+    desc1:
+      "Now regarding initiative, I have grown habitual of it and am always willing to go the extra mile and be a part of something new and add to my experiences list. Apart from my commitment towards my academics and my professional life I actually care for social causes and volunteering. Thus, as a member of the A&M Mechatronics ENSAM Casablanca Club, I joined the activities that promote the development of innovations. Having strong engineering fundamentals and constantly developing talents in Computer aided design using catia v5R21 and autodesk fusion 360, I am ready to use my capital to bring new value creation in technology field.",
+
+    className: "md:col-span-2", // change to md:col-span-2
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "./facebook.svg",
+    href: "https://web.facebook.com/houssam.clap/",
+  },
+  {
+    id: 2,
+    img: "./insta.svg",
+    href: "https://www.instagram.com/houssam_clap/",
+  },
+  {
+    id: 3,
+    img: "./git.svg",
+    href: "https://www.github.com/houssamcbk/",
+  },
+  {
+    id: 4,
+    img: "./link.svg",
+    href: "https://www.linkedin.com/in/houssam-choubik-5b4ba3316/",
+  },
+];
